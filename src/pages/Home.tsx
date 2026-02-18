7ee8e613b6bd60cc8b8668c5c1caf6faa90b3c6c
Richard Li <richardhli81@gmail.com>
2026-02-18 16:18:04 -0600

    added email

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
       <SlideIn direction="top" delay={200} duration={2000} className="z-10 relative">
         <div className="relative sm:ml-32 sm:mt-16 p-4 text-primary-reddish">
           <h1 className="text-5xl font-bold mb-4">richard li</h1>
-          <p className="text-[18px] mb-4">cs (& math) @ uw-madison</p>
+          <p className="text-[18px] leading-loose mb-1">cs (& math) @ uw-madison<br/>richardhli81@gmail.com</p>
           <ArrowButton label="github" to="https://github.com/li21rich/" />
           <ArrowButton label="linkedin" to="https://www.linkedin.com/in/richard-h-li/" />
         </div>
